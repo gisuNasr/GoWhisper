9ea016228a496deb6de3476ba86c6b6d6c162f18
gisu <f.nasroallahi@itoll.com>
2026-06-29 17:09:10 +0330

    add postgres migrations

---
+++ migrations/000001_init_schema.up.sql
@@ -0,0 +1,44 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users (
+   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+   username VARCHAR(50) UNIQUE NOT NULL,
+   first_name VARCHAR(50),
+   last_name VARCHAR(50),
+   created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+   updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+CREATE TABLE devices (
+   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+   user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+   identity_key_pub TEXT NOT NULL,
+   signed_pre_key_pub TEXT NOT NULL,
+   one_time_pre_keys JSONB NOT NULL,
+   created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+   updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+CREATE TABLE rooms (
+     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+     name VARCHAR(100),
+     type VARCHAR(20) NOT NULL CHECK (type IN ('pv', 'group')),
+     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+     updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+CREATE TABLE room_members (
+      room_id UUID NOT NULL REFERENCES rooms(id) ON DELETE CASCADE,
+      user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+      PRIMARY KEY (room_id, user_id)
+);
+
+CREATE TABLE messages (
+      id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+      room_id UUID NOT NULL REFERENCES rooms(id) ON DELETE CASCADE,
+      user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+      device_id UUID NOT NULL REFERENCES devices(id) ON DELETE CASCADE,
+      encrypted_payload TEXT NOT NULL,
+      created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+      updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
